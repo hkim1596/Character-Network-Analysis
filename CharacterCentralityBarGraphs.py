d248b98b72cceb3d63da09e01a6617743a1a75f9
Heejin Kim <hkim1596@gmail.com>
2023-11-06 00:38:42 +0900

    Updated the repository to match local state

--- CharacterCentralityBarGraphs.py
+++ CharacterCentralityBarGraphs.py
@@ -1,26 +1,34 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
+import os
 
 # Load the data from metrics_summary.csv
 results = pd.read_csv('output_metrics/metrics_summary.csv')
+
+# Ensure output directory exists
+os.makedirs('output_centrality_bar_graphs', exist_ok=True)
 
 # List of metrics to plot
 metrics = ['Degree Centrality', 'Betweenness Centrality', 'Eigenvector Centrality', 'Closeness Centrality']
 
 for metric in metrics:
-    # Sort the results DataFrame based on the metric in ascending order
-    sorted_results = results.sort_values(by=metric)
+    # Sort the results DataFrame based on the metric in descending order
+    sorted_results = results.sort_values(by=metric, ascending=False)
+
+    # Bar plot for each metric using Plotly
+    fig = px.bar(sorted_results, x='Play', y=metric, title=f'{metric} across Plays')
+
+    # Update layout
+    fig.update_layout(
+        xaxis_title="Play",
+        yaxis_title=metric,
+        xaxis={'categoryorder':'total descending'},
+        title=dict(text=f'{metric} across Plays', x=0.5),
+        autosize=True
+    )
     
-    plt.figure(figsize=(12, 6))
+    # Customize x-axis tick angle
+    fig.update_xaxes(tickangle=45)
     
-    # Bar plot for each metric
-    sorted_results.plot(x='Play', y=metric, kind='bar', legend=False)
-    
-    plt.title(f'{metric} across Plays')
-    plt.ylabel(metric)
-    plt.xlabel('Plays')
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    
-    plt.savefig(f'output_centrality_bar_graphs/{metric} Bar Graph.png', dpi=600)
-    #plt.show()
+    # Save the figure as an interactive HTML file
+    fig.write_html(f'output_centrality_bar_graphs/{metric}_Bar_Graph.html')
